aa116413c614253c5eee8324595a4891015c1b3a
begeric <begueteric@gmail.com>
2015-01-22 16:53:47 +0100

    nicer code

--- sublime-plugin/scalasublime.py
+++ sublime-plugin/scalasublime.py
@@ -1,16 +1,17 @@
 import sublime, sublime_plugin
 
 class LookupSymbolsCommand(sublime_plugin.TextCommand):
+
 	def run(self, edit):
 		sels = self.view.sel()
+		selsStr = map(lambda s: self.view.substr(s), sels)#[sel for sel in sels if self.view.substr(sel)]
 		#get all the selected words
-		#is it possible to remove self.view.substr(sel) redundancy?
-		onlyOneWords = [self.view.substr(sel) for sel in sels if self.view.substr(sel).find(' ') < 0] 
+		onlyOneWords = filter(lambda s: s.find(' ') < 0, selsStr)
 		value = ', '.join(onlyOneWords)
 		#get the word under the selection if no word have been selected
 		if not value: #please god forgive me, but 1) onlyOneWords is a generator 2) if I transform it to a List and thest not list it doesnt work :()
-			#duuude, gotta find a way to reduce that (139 chars!)
-			selectedWords = [self.view.substr(self.view.word(sel)) for sel in sels if self.view.substr(self.view.word(sel)).find(' ') < 0]
+			selsWord = map(lambda s: self.view.substr(self.view.word(s)), sels)
+			selectedWords = filter(lambda s: s.find(' ') < 0, selsWord)
 			value = ', '.join(selectedWords) 
 		self.view.set_status("scalasublime", value)
 	
